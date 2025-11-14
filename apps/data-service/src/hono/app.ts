75c2006de44777c275beae6bba7e832da1457dc8
Berend Vrakking <68348930+BerendVrakking@users.noreply.github.com>
2025-11-14 14:04:57 +0100

    Lesson - Geo Based Smart Routing completed

--- apps/data-service/src/hono/app.ts
+++ apps/data-service/src/hono/app.ts
@@ -1,5 +1,7 @@
 import { Hono } from "hono";
+import { cloudflareInfoSchema } from "@repo/data-ops/zod-schema/links";
 import { getLink } from "@repo/data-ops/queries/links";
+import { getDestinationForCountry } from "../helpers/route-ops";
 
 export const app = new Hono<{ Bindings: Env }>();
 
@@ -9,7 +11,20 @@ app.get("/:id", async (c) => {
     const { id } = c.req.param();
     
 
-    const linkInfoFromDb = await getLink(id);
+    const linkInfo = await getLink(id);
+    if (!linkInfo) {
+        return c.json({ error: "Destination not found" }, 404);
+    }
 
-    return c.json(linkInfoFromDb);
+    const cfHeader = cloudflareInfoSchema.safeParse(c.req.raw.cf);
+    if (!cfHeader.success) {
+        return c.text("Invalid Cloudflare Header", 400);
+    }
+
+    const headers = cfHeader.data;
+    const destination = getDestinationForCountry(linkInfo, headers.country);
+    console.log(destination);
+    
+    return c.redirect(destination);
+
 });
