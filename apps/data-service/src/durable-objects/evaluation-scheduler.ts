f68e504c32c90264320bf65ac4a8c45122a0ce27
Berend Vrakking <68348930+BerendVrakking@users.noreply.github.com>
2025-11-15 14:59:08 +0100

    Update alarm duration in EvaluationScheduler from 10 seconds to 1 day for improved scheduling accuracy.

--- apps/data-service/src/durable-objects/evaluation-scheduler.ts
+++ apps/data-service/src/durable-objects/evaluation-scheduler.ts
@@ -30,8 +30,8 @@ export class EvaluationScheduler extends DurableObject<Env>{
         await this.ctx.storage.put('clickData', this.clickData);
         const alarm = await this.ctx.storage.getAlarm();
         if (!alarm) {
-            const tenSeconds = moment().add(10, 'seconds').valueOf();
-            await this.ctx.storage.setAlarm(tenSeconds);
+            const oneDay = moment().add(1, 'day').valueOf();
+            await this.ctx.storage.setAlarm(oneDay);
         }
     }
     async alarm() {
