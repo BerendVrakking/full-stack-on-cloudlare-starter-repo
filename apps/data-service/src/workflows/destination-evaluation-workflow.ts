d4b2e5261a28126c9190fe8392f3d6964e98996d
Berend Vrakking <68348930+BerendVrakking@users.noreply.github.com>
2025-11-15 12:01:08 +0100

    Lesson - Saving AI output in our DB

--- apps/data-service/src/workflows/destination-evaluation-workflow.ts
+++ apps/data-service/src/workflows/destination-evaluation-workflow.ts
@@ -1,11 +1,14 @@
 import { WorkflowEntrypoint, WorkflowEvent, WorkflowStep } from 'cloudflare:workers';
 import { collectDestinationInfo } from '@/helpers/browser-render';
 import { aiDestinationChecker } from '@/helpers/ai-destination-checker';
+import { addEvaluation } from '@repo/data-ops/queries/evaluations';
+import { initDatabase } from '@repo/data-ops/database';
 
 
 
 export class DestinationEvaluationWorkflow extends WorkflowEntrypoint<Env, DestinationStatusEvaluationParams> {
     async run(event: Readonly<WorkflowEvent<DestinationStatusEvaluationParams>>, step: WorkflowStep) {
+        initDatabase(this.env.DB);
 
         const collectedData = await step.do("Collect rendered destination page data", async () => {
             return collectDestinationInfo(this.env, event.payload.destinationUrl);
@@ -18,6 +21,17 @@ export class DestinationEvaluationWorkflow extends WorkflowEntrypoint<Env, Desti
             return aiDestinationChecker(this.env, collectedData.bodyText);
         });
 
+
+        const evaluationId = await step.do("Save evalutaition in database", async () => {
+            return await addEvaluation({
+                linkId: event.payload.linkId,
+                status: aiStatus.status,
+                reason: aiStatus.statusReason,
+                accountId: event.payload.accountId,
+                destinationUrl: event.payload.destinationUrl,
+            })
+        });
         console.log("AI Status:", aiStatus);
+        console.log("Evaluation ID:", evaluationId);
     }
 }
